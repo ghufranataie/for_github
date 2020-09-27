ced77585a0f235ee337555c355cb7ddd8edab65e
ghufranataie <ghufranataie@hotmail.com>
2020-09-27 21:47:37 +0430

    Initial commit

---
+++ android/app/src/main/java/com/example/for_github/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.for_github;
+
+import android.support.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
